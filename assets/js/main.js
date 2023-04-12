a65fdf896e58ace915e5b053cca87cb881562e7f
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-04-12 15:16:09 -0300

    add js qualificações
    adicionado e finalizado campo de 'qualificações' no js

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,11 +41,32 @@ function toggleSkills(){
         skillsContent[i].className = 'skills_content skills_close'
     }
     if(itemClass === 'skills_content skills_close'){
-        this.parentNode.className = 'skills_content skills_open'
+        this.parentNode.className = 'skills_co ntent skills_open'
     }
 }
 
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
     
+})
+
+// qualificações
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
 })
