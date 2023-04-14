dc9cea4eeb3da60abff3e3bd2e97926ef2874ad1
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-04-14 11:52:11 -0300

    att js e css
    finalizado a sessão ''serviços'' no .css e .js

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,24 @@ tabs.forEach(tab =>{
 })
 
 //serviços
+
+const modalViews = document.querySelectorAll('.services_modal'),
+    modalBtns = document.querySelectorAll('.services_button'),
+    modalCloses = document.querySelectorAll('.services_modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
